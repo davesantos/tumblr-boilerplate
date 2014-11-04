9756f7ac5067a0132c8ad97fcc5617056fb78bf9
Dave Santos <info@davesantos.com>
2014-11-04 00:53:40 -0800

    Added UA

--- js/all.js
+++ js/all.js
@@ -1,5 +1,38 @@
-$(document).ready(function() {
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-});
+ga('create', 'UA-4820278-13', 'auto');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
 
+//http://www.axllent.org/docs/view/track-outbound-links-with-analytics-js/
+function _gaLt(event){
+    var el = event.srcElement || event.target;
+
+    /* Loop up the tree through parent elements if clicked element is not a link (eg: an image inside a link) */
+    while(el && (typeof el.tagName == 'undefined' || el.tagName.toLowerCase() != 'a' || !el.href))
+        el = el.parentNode;
+
+    if(el && el.href){
+        if(el.href.indexOf(location.host) == -1){ /* external link */
+            ga("send", "event", "Outgoing Links", el.href, document.location.pathname + document.location.search);
+            /* if target not set then delay opening of window by 0.5s to allow tracking */
+            if(!el.target || el.target.match(/^_(self|parent|top)$/i)){
+                setTimeout(function(){
+                    document.location.href = el.href;
+                }.bind(el),500);
+                /* Prevent standard click */
+                event.preventDefault ? event.preventDefault() : event.returnValue = !1;
+            }
+        }
+
+    }
+}
+
+/* Attach the event to all clicks in the document after page has loaded */
+var w = window;
+w.addEventListener ? w.addEventListener("load",function(){document.body.addEventListener("click",_gaLt,!1)},!1)
+  : w.attachEvent && w.attachEvent("onload",function(){document.body.attachEvent("onclick",_gaLt)});
