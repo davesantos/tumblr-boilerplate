1bebc24c6c7979847c57377675ed15b76ce42598
Dave Santos <info@davesantos.com>
2014-11-05 21:49:22 -0800

    Compressed

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ sass_dir = "_sass"
 images_dir = "assets"
 javascripts_dir = "js"
 
-output_style = :expanded
+output_style = :compressed
 environment = :production
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
